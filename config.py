62012bfaf5d2eeb5bb72110fc80b8bbefbec80cd
psujhord2011 <jhorden.lashley@gmail.com>
2026-05-18 07:20:30 -0400

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,13 @@ WATCH = {
     "types": ["PREMIUM", "PREFERRED", "FULL_HOOKUP"]
 }
 
-DISCORD_WEBHOOK = ""  # optional
+import os
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+SMTP_USERNAME = os.environ["SMTP_USERNAME"]
+SMTP_PASSWORD = os.environ["SMTP_PASSWORD"]
+
+EMAIL_TO = "3016978093@txt.att.net"
+EMAIL_SUBJECT = "Fort Wilderness Alert"
+EMAIL_BODY = "Campsite available for your selected dates!"
